3feadae7d2ea268214745168f2a4c25aaa05a153
Jakub Sadowski <js@vcc.earth>
2019-10-19 21:08:36 +0100

    adding proper argument handling for barcode daemon

--- barcode/install.sh
+++ barcode/install.sh
@@ -2,9 +2,12 @@
 (cd /opt && rm -r barcode-daemon && git clone https://github.com/critBit95/barcode-daemon.git)
 
 # install daemon
-cp barcode-daemon.service /etc/systemd/system/.
+cp barcode-daemon@.service /etc/systemd/system/.
 
-#systemctl enable barcode-daemon@/dev/input/by-id/usb-GASIA_PS2toUSB_Adapter-event-kbd.service
-systemctl enable barcode-daemon.service
+SCANNER_PATH=$(systemd-escape "/dev/input/by-id/usb-GASIA_PS2toUSB_Adapter-event-kbd")
+
+
+systemctl enable barcode-daemon@$SCANNER_PATH.service
+#systemctl enable barcode-daemon@lol.service
 
 
--- slides/slides.sh
+++ slides/slides.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-fim -a -d /dev/fb0 /home/pi/images/* -c 'while(1){display;sleep "500";next;}'
+fim -a -d /dev/fb0 /home/pi/images/* -c 'while(1){display;sleep "30";next;}'
 
